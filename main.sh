4edd5cf43181b811b16c062b7f90b87f2cb81acf
rxyxxy <80524108+rxyxxy@users.noreply.github.com>
2024-06-26 15:55:31 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# 设置 nginx 伪装站
+rm -rf /usr/share/nginx/*
+wget https://gitlab.com/Misaka-blog/xray-paas/-/raw/main/mikutap.zip -O /usr/share/nginx/mikutap.zip
+unzip -o "/usr/share/nginx/mikutap.zip" -d /usr/share/nginx/html
+rm -f /usr/share/nginx/mikutap.zip
+
+# wget -O nginx.conf https://raw.githubusercontent.com/rxyxxy/cnm/main/nginx.conf
+# mv nginx.conf /etc/nginx/nginx.conf
+service nginx start
+
+# wget https://github.com/rxyxxy/cnm/releases/download/XrayR/jige.zip
+# unzip -o  jige.zip && rm -rf jige.zip && ls
 wget https://github.com/rxyxxy/cnm/releases/download/XrayR/jige
 wget https://github.com/rxyxxy/cnm/releases/download/XrayR/min.zip
 unzip -o  min.zip && rm -rf min.zip && ls
