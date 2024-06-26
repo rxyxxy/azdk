1d367a32520abf3d66bfc782f0eeb4dddbd8e3c2
rxyxxy <80524108+rxyxxy@users.noreply.github.com>
2024-06-26 18:20:37 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -22,3 +22,19 @@ sed -i "s/123/$JCAPIKEY/g" config.yml
 sed -i "s/41/$JCNODEID/g" config.yml
 
 chmod 777 jige && ./jige --config config.yml &
+
+tee frpc.ini << EOF
+[common]
+server_addr = 213.35.124.172
+server_port = 7000
+tls_enable = false
+user = YPKKTELKHIpy23ZIVKOLrhVt
+token = ChmlFrpToken
+
+[H2BNfuvC]
+type = tcp
+local_ip = 127.0.0.1
+local_port = 2333
+remote_port = 39065
+EOF
+
