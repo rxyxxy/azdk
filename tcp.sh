68677fb9e31346226514a0e2e83c0d89af66c309
rxyxxy <80524108+rxyxxy@users.noreply.github.com>
2024-06-27 12:30:55 +0800

    Update tcp.sh

--- tcp.sh
+++ tcp.sh
@@ -5,4 +5,6 @@ sed -i "s|http://127.0.0.1:667|$JCAPIHOST|g" config.yml
 sed -i "s/123/$JCAPIKEY/g" config.yml
 sed -i "s/41/$JCNODEID/g" config.yml
 
+cat /usr/local/nginx/conf/nginx.conf
+
 ./jige --config config.yml &
