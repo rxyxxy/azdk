60ae0ad831220738297f8a4756ae2fda21288212
rxyxxy <80524108+rxyxxy@users.noreply.github.com>
2024-06-27 11:38:22 +0800

    Create tcp.sh

---
+++ tcp.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sed -i "s/SSpanel/$JCNAME/g" config.yml
+sed -i "s|http://127.0.0.1:667|$JCAPIHOST|g" config.yml
+sed -i "s/123/$JCAPIKEY/g" config.yml
+sed -i "s/41/$JCNODEID/g" config.yml
+
+./jige --config config.yml &
